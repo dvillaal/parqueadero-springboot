5c22154f2436c6bd5018a2364a10e9e6937e9d86
dvillaal <dvillaal@unal.edu.co>
2026-01-06 17:09:31 -0500

    remover uso de Lombok y agregar constructores en los manejadores de entrada y salida

--- src/main/java/com/david/parqueadero/infraestructure/handlers/RegistrarEntradaHandler.java
+++ src/main/java/com/david/parqueadero/infraestructure/handlers/RegistrarEntradaHandler.java
@@ -2,13 +2,15 @@ package com.david.parqueadero.infraestructure.handlers;
 
 import com.david.parqueadero.domain.model.enums.TipoVehiculo;
 import com.david.parqueadero.domain.useCases.RegistrarEntrada.RegistrarEntradaUseCase;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class RegistrarEntradaHandler {
     private final RegistrarEntradaUseCase registrarEntradaUseCase;
+
+    public RegistrarEntradaHandler(RegistrarEntradaUseCase registrarEntradaUseCase) {
+        this.registrarEntradaUseCase = registrarEntradaUseCase;
+    }
 
     public void handle(String placa, TipoVehiculo tipoVehiculo) {
         registrarEntradaUseCase.registrarEntrada(placa, tipoVehiculo);
--- src/main/java/com/david/parqueadero/infraestructure/handlers/RegistrarSalidaHandler.java
+++ src/main/java/com/david/parqueadero/infraestructure/handlers/RegistrarSalidaHandler.java
@@ -1,13 +1,15 @@
 package com.david.parqueadero.infraestructure.handlers;
 
 import com.david.parqueadero.domain.useCases.RegistrarSalida.RegistrarSalidaUseCase;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class RegistrarSalidaHandler {
     private final RegistrarSalidaUseCase registrarSalidaUseCase;
+
+    public RegistrarSalidaHandler(RegistrarSalidaUseCase registrarSalidaUseCase) {
+        this.registrarSalidaUseCase = registrarSalidaUseCase;
+    }
 
     public void handle(String placa) {
         registrarSalidaUseCase.registrarSalida(placa);
